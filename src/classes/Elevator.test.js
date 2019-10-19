3a6bb8118836cbb1e0f8fd2f6442aad9fb99c825
hunibotha <huni.developer@yahoo.com>
2019-10-20 00:48:49 +0300

    move more important tests up inside Elevator.test.js

--- src/classes/Elevator.test.js
+++ src/classes/Elevator.test.js
@@ -7,60 +7,6 @@ const passenger2 = new Passenger()
 const passenger3 = new Passenger()
 const passenger4 = new Passenger()
 
-test("Test Elevator.direction", function () {
-  const elevator = new Elevator(undefined, 3, 4)
-  
-  expect(elevator.direction).toEqual(Direction.DIRECTIONS.UP)
-  
-  elevator.destinationFloor = 2
-  expect(elevator.direction).toEqual(Direction.DIRECTIONS.DOWN)
-  
-  elevator.currentFloor = 2
-  expect(elevator.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
-  
-  const elevator2 = new Elevator()
-  expect(elevator2.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
-})
-
-test("Test Elevator.AssignPassenger", function () {
-  const elevator = new Elevator(
-    undefined,
-    undefined,
-    undefined,
-    [passenger1, passenger2, passenger3]
-  )
-  
-  elevator.AssignPassenger(passenger4)
-  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger3, passenger4])
-})
-
-test("Test Elevator.AssignPassengers", function () {
-  const elevator = new Elevator(
-    undefined,
-    undefined,
-    undefined,
-    [passenger1, passenger2]
-  )
-  
-  elevator.AssignPassengers([passenger3, passenger4])
-  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger3, passenger4])
-})
-
-test("Test Elevator.UnAssignPassenger", function () {
-  const elevator = new Elevator(
-    undefined,
-    undefined,
-    undefined,
-    [passenger1, passenger2, passenger3, passenger4]
-  )
-  
-  elevator.UnAssignPassenger(passenger3)
-  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger4])
-  
-  elevator.UnAssignPassenger(passenger3)
-  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger4])
-})
-
 test(
   "Test Elevator.GetTimeToPassenger when elevator has to finish the current delivery before getting back to the passenger",
   function () {
@@ -302,4 +248,58 @@ test("Test Elevator.IsMovingInTheDirectionOfThePassenger when elevator is standi
   
   const passengerOnALowerFloor = new Passenger(undefined, 3)
   expect(elevator.IsMovingInTheDirectionOfThePassenger(passengerOnALowerFloor)).toEqual(false)
+})
+
+test("Test Elevator.direction", function () {
+  const elevator = new Elevator(undefined, 3, 4)
+  
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.UP)
+  
+  elevator.destinationFloor = 2
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.DOWN)
+  
+  elevator.currentFloor = 2
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
+  
+  const elevator2 = new Elevator()
+  expect(elevator2.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
+})
+
+test("Test Elevator.AssignPassenger", function () {
+  const elevator = new Elevator(
+    undefined,
+    undefined,
+    undefined,
+    [passenger1, passenger2, passenger3]
+  )
+  
+  elevator.AssignPassenger(passenger4)
+  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger3, passenger4])
+})
+
+test("Test Elevator.AssignPassengers", function () {
+  const elevator = new Elevator(
+    undefined,
+    undefined,
+    undefined,
+    [passenger1, passenger2]
+  )
+  
+  elevator.AssignPassengers([passenger3, passenger4])
+  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger3, passenger4])
+})
+
+test("Test Elevator.UnAssignPassenger", function () {
+  const elevator = new Elevator(
+    undefined,
+    undefined,
+    undefined,
+    [passenger1, passenger2, passenger3, passenger4]
+  )
+  
+  elevator.UnAssignPassenger(passenger3)
+  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger4])
+  
+  elevator.UnAssignPassenger(passenger3)
+  expect(elevator.assignedPassengers).toEqual([passenger1, passenger2, passenger4])
 })
