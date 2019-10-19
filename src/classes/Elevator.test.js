b2702aff56847e76beef936b6aa33fae773de2bf
hunibotha <huni.developer@yahoo.com>
2019-10-19 17:54:34 +0300

    test Elevator.direction

--- src/classes/Elevator.test.js
+++ src/classes/Elevator.test.js
@@ -1,10 +1,26 @@
 import Elevator from "./Elevator"
 import Passenger from "./Passenger"
+import Direction from "./Direction"
 
 const passenger1 = new Passenger()
 const passenger2 = new Passenger()
 const passenger3 = new Passenger()
 const passenger4 = new Passenger()
+
+test("Test Elevator.direction", function () {
+  const elevator = new Elevator(undefined, 3, 4)
+  
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.UP)
+  
+  elevator.destinationFloor = 2
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.DOWN)
+  
+  elevator.currentFloor = 2
+  expect(elevator.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
+  
+  const elevator2 = new Elevator()
+  expect(elevator2.direction).toEqual(Direction.DIRECTIONS.NO_DIRECTION)
+})
 
 test("Test Elevator.AssignPassenger", function () {
   const elevator = new Elevator(
